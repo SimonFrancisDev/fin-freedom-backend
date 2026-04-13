8eb54af4294886c8fa51a643a2c677857ec2cf79
SimonFrancisDev <francischukwuemeka10@gmail.com>
2026-04-13 14:25:11 +0100

    Just a tiny bug fix

--- src/controllers/receiptController.js
+++ src/controllers/receiptController.js
@@ -1,4 +1,3 @@
-
 import {
   fetchReceiptsByAddress,
   fetchReceiptsByActivationId,
@@ -39,46 +38,3 @@ export async function getReceiptsByActivationId(req, res, next) {
     next(error);
   }
 }
-
-
-
-
-
-
-
-
-
-// import {
-// 
-
-fetchReceiptsByAddress,
-//   fetchReceiptsByActivationId,
-// } from '../services/read/receiptQueryService.js';
-
-// export async function getReceiptsByAddress(req, res, next) {
-//   try {
-//     const { address } = req.params;
-//     const data = await fetchReceiptsByAddress(address, req.query);
-
-//     res.status(200).json({
-//       ok: true,
-//       ...data,
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// }
-
-// export async function getReceiptsByActivationId(req, res, next) {
-//   try {
-//     const { activationId } = req.params;
-//     const data = await fetchReceiptsByActivationId(activationId, req.query);
-
-//     res.status(200).json({
-//       ok: true,
-//       ...data,
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// }
