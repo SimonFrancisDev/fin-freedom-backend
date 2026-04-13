5fb167f4539b99605f171a136e8f1cb340c43405
SimonFrancisDev <francischukwuemeka10@gmail.com>
2026-04-13 18:18:32 +0100

    still fixing app.js-v3

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ app.use(
 );
 
 // app.options('*', cors());
-app.options('/:any*', cors());
+// app.options('/:any*', cors());
 
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
